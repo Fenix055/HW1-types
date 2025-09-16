bb43cc11e1b3eb8222791c2da163a74afda434bd
Fenix055 <DevilHanter05@gmail.com>
2025-09-16 20:50:38 +0300

    fix wrong type of i in print method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void print(Object... args){
-        for (int i=0; i< args.length; i++) {
+        for (byte i=0; i< args.length; i++) {
             System.out.println(args[i]);
         }
         System.out.println();
